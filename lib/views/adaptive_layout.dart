a3c688ce2279d695a13910cc3498ee35eab14396
YaseenHussein <tfuytf775@gmail.com>
2024-04-28 22:23:16 +0300

    first commit

---
+++ lib/views/adaptive_layout.dart
@@ -0,0 +1,27 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+
+class AdaptiveLayout extends StatelessWidget {
+  const AdaptiveLayout({
+    super.key,
+    required this.mobileLayout,
+    required this.deskTopLayout,
+    required this.tabletLayout,
+  });
+  final WidgetBuilder mobileLayout, deskTopLayout, tabletLayout;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return mobileLayout(context);
+        } else if (constraints.maxWidth < 900) {
+          return tabletLayout(context);
+        } else {
+          return deskTopLayout(context);
+        }
+      },
+    );
+  }
+}
